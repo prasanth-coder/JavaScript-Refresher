012cf13814ba7c0dc8279629aa002108e866d229
prasanth-coder <prasanthmuthupandi@gmail.com>
2024-02-10 03:43:58 +0530

    minor changes

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,5 +1,13 @@
 const nums = [{"name" : "prasanth" ,"rollNo" : 100},{"name" : "prasanna" ,"rollNo" : 101},{"name" : "prakash" ,"rollNo" : 103}]
+const n = {"name" : "prasanthsdsd" ,"rollNo" : 1000};
 
+const printDetials = (n) =>{
+    const {name,rollNo} = n;
+    console.log(name+" ------ "+rollNo);
+
+}
+
+printDetials(n);
 console.log(nums.map(o => o.name))
 console.log(nums.filter(o => o.rollNo === 100))
 
@@ -8,3 +16,5 @@ console.log(nums.reduce((acc,total) => acc+total.rollNo,0))
 console.log(nums.some(o => o.name === "prasanth"));
 
 console.log(nums.every(o => o.name === "prasanth"));
+
+console.log()
